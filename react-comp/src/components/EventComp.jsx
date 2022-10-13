7559b5a6c6b8a5c7549d8b086c869d535c20cf1d
hj-Seong <hjseong1222@gmail.com>
2022-10-13 16:41:26 +0900

    1013 이벤트-클래스컴포넌트-함수-2

--- react-comp/src/components/EventComp.jsx
+++ react-comp/src/components/EventComp.jsx
@@ -5,9 +5,12 @@ class EventComp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isToggleOn:true
+            isToggleOn:true,
+            num :1,
         }
+
         this.handleClick = this.handleClick.bind(this);
+        this.numDouble = this.numDouble.bind(this);
     }
 
     // 메소드로만 사용하면 this의 위치를 찾지못해서 오류발생 : this를 bind로 연결
@@ -18,13 +21,21 @@ class EventComp extends Component {
         this.setState({isToggleOn :!this.state.isToggleOn})
     }
 
+    // 값을 두배로 만들어주는 함수(메소드)
+    numDouble() {
+        this.setState({num : this.state.num * 2})
+    }
+
+
     render() {
-        const {isToggleOn} = this.state;
+        const {isToggleOn, num} = this.state;
         return (
             <div>
                 <button onClick={ this.handleClick }> { isToggleOn ? 'ON' : 'OFF' } </button>
                 {/** num = 1이라는 숫자값을 추가해서, 버튼을 클릭할때마다 2배로 늘어날수있게 */}
                 {/* 값은 버튼에 출력 */}
+
+                <button onClick={ this.numDouble }>{num}</button>
             </div>
         )
     }
