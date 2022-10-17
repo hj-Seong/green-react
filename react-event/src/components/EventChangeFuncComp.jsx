2c137c2f4b055d85ac3e70b21e33f923b85e5ca9
hj-Seong <hjseong1222@gmail.com>
2022-10-17 12:52:50 +0900

    1017 이벤트-공통change 함수

--- react-event/src/components/EventChangeFuncComp.jsx
+++ react-event/src/components/EventChangeFuncComp.jsx
@@ -3,21 +3,26 @@
 import { useState } from "react";
 
 const EventChangeFuncComp = () => {
-    const [message, setMessage] = useState("안녕");
+    // set함수로 접근하기 위해서 하나의 useState에 객체로 생성
+    const [message, setMessage] = useState({
+        message : "안녕",
+        name : "성춘향"
+    });
 
     // onchange 함수로 바꿔서 사용하기
-
+    // 이대로 사용하면 문자열이 통채로 들어감 > 수정 필요
+    const onchange = (e) => {
+        console.log(e.target.value);
+        setMessage(e.target.value);
+    }
     return (
         <div>
             {/** h1태그에 input으로 작성한 내용 출력하기 */}
             <h1>글을 입력하세요</h1>
-            <input type="text" onChange={
-                (e) => {
-                    console.log(e.target.value);
-                    setMessage(e.target.value);
-                }
-            } />
-            <p>{message}</p>
+            <input type="text" onChange={ onchange } />
+            <p>{message.message}</p>
+            <input type="text" onChange={ onchange } />
+            <p>{message.name}</p>
         </div>
     )
 }
