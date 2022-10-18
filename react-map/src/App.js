3c7814a9187e8fac5f757366cd96932a95d0a563
hj-Seong <hjseong1222@gmail.com>
2022-10-18 11:46:22 +0900

    1018 컴포넌트 반복-데이터추가

--- react-map/src/App.js
+++ react-map/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MapComp from './components/MapComp';
+import MapCompTest from './components/MapCompTest';
 
 function App() {
   return (
     <div className="App">
       <MapComp />
+      <MapCompTest />
     </div>
   );
 }
