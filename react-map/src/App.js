29dfa719b898ac258d24c4faf9d7a6be5a3b11c9
hj-Seong <hjseong1222@gmail.com>
2022-10-18 17:21:17 +0900

    1018 클래스네임

--- react-map/src/App.js
+++ react-map/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import MapComp from './components/MapComp';
 import MapCompTest from './components/MapCompTest';
 import LifeCycleComp from './components/LifeCycleComp';
+import ClassNameComp from './components/ClassNameComp';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <MapComp />
       <MapCompTest />
       <LifeCycleComp />
+      <ClassNameComp />
     </div>
   );
 }
