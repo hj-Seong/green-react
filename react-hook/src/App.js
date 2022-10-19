43a9af2bca2eb8b44af48e690892a3b5d27e0ed9
hj-Seong <hjseong1222@gmail.com>
2022-10-19 15:36:29 +0900

    1019 hook-memo

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import StateEffectHookComp from './components/StateEffectHookComp';
 import ReducerComp from './components/ReducerComp';
+import UseMemoComp from './components/UseMemoComp';
 
 function App() {
   return (
     <div className="App">
       <StateEffectHookComp />
       <ReducerComp />
+      <UseMemoComp />
     </div>
   );
 }
