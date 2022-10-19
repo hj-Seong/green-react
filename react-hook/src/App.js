2890e462413e1982f110af1932fea9196f6329aa
hj-Seong <hjseong1222@gmail.com>
2022-10-19 12:20:35 +0900

    1019 hook-reducer-1

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import StateEffectHookComp from './components/StateEffectHookComp';
+import ReducerComp from './components/ReducerComp';
 
 function App() {
   return (
     <div className="App">
       <StateEffectHookComp />
+      <ReducerComp />
     </div>
   );
 }
