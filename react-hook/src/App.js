c2597da18abaacce28f58e1abd98c657169a4a40
hj-Seong <hjseong1222@gmail.com>
2022-10-19 10:40:01 +0900

    1019 hook-state와 effect

---
+++ react-hook/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StateEffectHookComp from './components/StateEffectHookComp';
+
+function App() {
+  return (
+    <div className="App">
+      <StateEffectHookComp />
+    </div>
+  );
+}
+
+export default App;
