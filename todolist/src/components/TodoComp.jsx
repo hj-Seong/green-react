07b3a37bcffc736a78e5c2dbbd9f90ab8a84c00e
hj-Seong <hjseong1222@gmail.com>
2022-10-20 16:41:30 +0900

    1020 todo-todocomp-checkedlist

--- todolist/src/components/TodoComp.jsx
+++ todolist/src/components/TodoComp.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import TodoItemComp from "./TodoItemComp";
 
 const initstate = {
     // todoitem의 형태 확인 : 1개의 todo값을 가지고 있음
@@ -8,7 +9,18 @@ const initstate = {
         id : 1
     },
     // 배열을 통해서 여러개의 todoitem 사용
-    todolist : [], // todoitem을 넣을 공간
+    todolist : [
+        {
+            done : true,
+            todo : "첫번째 할일",
+            id : 1
+        },
+        {
+            done : false,
+            todo : "두번째 할일",
+            id : 2
+        }
+    ], // todoitem을 넣을 공간
     // todo값을 입력받을 todoInput
     todoInput : ""
 }
@@ -21,9 +33,28 @@ function reducer(state, action) {
                     ...state.todoitem,
                     done: !state.todoitem.done
                 }
-            }
+            };
+        case "checkedlist":
+            // 리스트 안에서 객체 하나를 찾아서, 그 객체의 done을 !값으로 바꾸어줌
+            // 객체 하나를 찾는 방법 : id을 통해 찾음
+            const newTodoList = state.todolist.map( (todoitem)=> {
+                if ( todoitem.id == action.id ) {
+                    // 새로 객체를 만들어주어서, done 바꾸어줌
+                    todoitem = {
+                        ...todoitem,
+                        done : !todoitem.done
+                    }
+                }
+                return todoitem
+            } )
+            // 
+            const newTodoList2 = state.todolist.map((todoitem)=>(todoitem.id == action.id ?  {...todoitem, done : !todoitem.done} : todoitem ))
+            return {...state, todolist : newTodoList}
         case "todoInput":
-            return { ...state , todoInput : action.payload}
+            return { ...state , todoInput : action.payload};
+        default : // 다른 값이 들어왔을때 현재 state를 유지하고 오류를 알려준다.
+            console.error("존재 하지 않는 액션타입입니다")
+            return state;
     }
 
 }
@@ -41,10 +72,12 @@ const TodoComp = () => {
                     {/** TodoItem - 체크여부, 할일, id값(삭제에 필요) */}
                     <input type="checkbox" 
                         checked={state.todoitem.done} 
-                        onClick={()=>{dispatch({type: "checked"})}} />
+                        onClick={()=>{dispatch({type: "checked"})}} readOnly />
                         {state.todoitem.todo}
                     <button>X</button>
                 </li>
+                {/** TodoItemComp를 만들어 map을 통해서 내용 출력 */}
+                { state.todolist.map((todoitem)=>(<TodoItemComp todoitem={todoitem} dispatch={dispatch} />))}
             </ul>
         </div>
     );
