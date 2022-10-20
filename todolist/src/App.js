a233f45cc31427e7c24a2ac6dc99bb7579c29de0
hj-Seong <hjseong1222@gmail.com>
2022-10-20 14:38:52 +0900

    1020 todo-clock

---
+++ todolist/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ClockComp from './components/ClockComp';
+
+function App() {
+  return (
+    <div className="App">
+      <ClockComp />
+    </div>
+  );
+}
+
+export default App;
